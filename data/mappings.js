3d4aa04441aef209ee8f1b4d1f7c29fa6ded29a8
geekypenguin <github@matthewbeddow.co.uk>
2025-09-14 11:21:57 +0100

    New numbers and usage

---
+++ data/mappings.js
@@ -0,0 +1,252 @@
+const mappings = {
+    "announcement": {
+        properties: {
+            folder: "announcement",
+            priority: 2,
+        },
+        announcements : {
+            "1": {
+                "name": "BOARDING"
+            },
+            "2": {
+                "name": "BOARDING (REFUELING)"
+            },
+            "3": {
+                "name": "BOARDING COMPLETE"
+            },
+            "4": {
+                "name": "DELAYED BOARDING"
+            },
+            "5": {
+                "name": "NUT ALLERGY"
+            },
+            "6": {
+                "name": "EXTRA LEGROOM £10"
+            },
+            "7": {
+                "name": "EXTRA LEGROOM £20"
+            },
+			"8": {
+                "name": "ARM DOORS"
+            },
+            "9": {
+                "name": "SAFETY DEMO: B733"
+            },
+            "10": {
+                "name": "SAFETY DEMO: B738"
+            },
+            "11": {
+                "name": "SAFETY DEMO: A321"
+            },
+            "12": {
+                "name": "SAFETY DEMO: A21N"
+            },
+            "13": {
+                "name": "CABIN LIGHTS OFF (AUTOMATIC)"
+            },
+            "14": {
+                "name": "CABIN CREW RELEASED"
+            },
+            "15": {
+                "name": "CABIN CREW RELEASED - LATE"
+            },
+            "16": {
+                "name": "INFLIGHT SERVICE 1"
+            },
+            "17": {
+                "name": "INFLIGHT SERVICE NO FRESH"
+            },
+            "18": {
+                "name": "WASTE COLLECTION"
+            },
+            "19": {
+                "name": "CHARITY GAMECARDS"
+            },
+            "20": {
+                "name": "INFLIGHT SERVICE 2"
+            },
+            "21": {
+                "name": "INFLIGHT SERVICE 2 NO FRESH"
+            },
+		    "22": {
+                "name": "INFLIGHT RETAIL"
+            },
+            "24": {
+                "name": "Final Stretch"
+            },
+            "25": {
+                "name": "DESCENT (NORMAL)"
+            },
+            "26": {
+                "name": "DESCENT (LOW VIS)"
+            },
+            "27": {
+                "name": "HOLIDAYS ARRIVAL"
+            },
+            "28": {
+                "name": "HOLIDAYS ARRIVAL CONJESTION"
+            },	
+            "29": {
+                "name": "GENERIC ARRIVAL"
+            },			
+            "30": {
+                "name": "BFS ARRIVAL"
+            },
+            "31": {
+                "name": "BFS ARRIVAL ALT"
+            },
+            "32": {
+                "name": "BHX ARRIVAL"
+            },
+            "33": {
+                "name": "BHX ARRIVAL ALT"
+            },
+            "34": {
+                "name": "BOH ARRIVAL"
+            },
+            "35": {
+                "name": "BOH ARRIVAL ALT"
+            },
+            "36": {
+                "name": "BFS ARRIVAL"
+            },
+            "37": {
+                "name": "BFS ARRIVAL ALT"
+            },
+            "38": {
+                "name": "EDI ARRIVAL"
+            },
+			"39": {
+                "name": "EDI ARRIVAL ALT"
+            },
+            "40": {
+                "name": "EMA ARRIVAL"
+            },
+            "41": {
+                "name": "EMA ARRIVAL ALT"
+            },
+            "42": {
+                "name": "GLA ARRIVAL"
+            },
+            "43": {
+                "name": "GLA ARRIVAL ALT"
+            },
+            "44": {
+                "name": "LBA ARRIVAL"
+            },
+            "45": {
+                "name": "LBA ARRIVAL ALT"
+            },
+            "46": {
+                "name": "LTN ARRIVAL"
+            },
+            "47": {
+                "name": "LTN ARRIVAL ALT"
+            },
+            "48": {
+                "name": "LGW ARRIVAL"
+            },
+			"49": {
+                "name": "LGW ARRIVAL ALT"
+            },
+            "50": {
+                "name": "LPL ARRIVAL"
+            },
+            "51": {
+                "name": "LPL ARRIVAL ALT"
+            },
+            "52": {
+                "name": "MAN ARRIVAL"
+            },
+            "53": {
+                "name": "MAN ARRIVAL ALT"
+            },
+            "54": {
+                "name": "NCL ARRIVAL"
+            },
+            "55": {
+                "name": "NCL ARRIVAL ALT"
+            },
+            "56": {
+                "name": "STN ARRIVAL"
+            },
+            "57": {
+                "name": "STN ARRIVAL ALT"
+            },
+            "58": {
+                "name": "HOME ARRIVAL"
+            },
+			"59": {
+                "name": "HOME ARRIVAL ALT"
+            },
+            "60": {
+                "name": "DIVERSION ARRIVAL"
+            },
+            "61": {
+                "name": "Doors to Manual"
+            },
+			"62": {
+                "name": "FINAL DOORS CHECK"
+            },
+			"63": {
+                "name": "FRONT EXIT ONLY"
+            },
+            "64": {
+                "name": "FRONT AND REAR EXIT"
+            },
+            "65": {
+                "name": "REAR THEN FRONT EXIT"
+            },
+            "71": {
+                "name": "TURBULENCE"
+            },
+            "72": {
+                "name": "DISINSECTION"
+            },
+            "74": {
+                "name": "BAGGAGE REMINDER"
+            }
+         },
+    },
+    "music": {
+        properties: {
+            folder: "music",
+            priority: 3,
+        },
+        announcements: {
+            "1": {
+                name: "Jess Glynne"
+            },
+			"2": {
+                name: "BOARDING Playlist"
+            },
+			"3 {
+				name: "Disembarking Playlist - UK"
+            },
+			"4": {
+                name: "Disembarking Playlist - EU"
+            },
+			"5": {
+                name: "Christmas Playlist"
+            }
+        },
+    },
+    "flight": {
+        properties: {
+            folder: "flight",
+            priority: 1,
+        },
+        announcements: {
+            "1": {
+                name: "B737 CABIN PHONE"
+            },
+            "2": {
+                name: "B752 CABIN PHONE"
+            },
+            "3": {
+                name: "B752 CABIN PHONE"
+            }
+        },
+    }
+}
+
